0eeb458837b314de0414848ea7f030ef5085cc00
rhyous <rhyous@yahoo.com>
2016-12-05 12:44:35 -0800

    Added ServiceCommonSearchable

---
+++ Rhyous.WebFramework/Services.Common/ServiceCommonSearchable.cs
@@ -0,0 +1,38 @@
+﻿using Rhyous.WebFramework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Rhyous.WebFramework.Services
+{
+    public abstract class ServiceCommonSearchable<T, Tinterface> : ServiceCommon<T, Tinterface>, ISearchableServiceCommon<T, Tinterface>
+        where T : class, Tinterface
+    {
+        public abstract Expression<Func<T, string>> PropertyExpression { get; }
+
+        public Tinterface Get(string stringProperty)
+        {
+            return Repo.Get(stringProperty, PropertyExpression);
+        }
+
+        public List<Tinterface> Search(string stringProperty)
+        {
+            return Repo.Search(stringProperty, PropertyExpression);
+        }
+
+        public override List<Tinterface> Add(IList<Tinterface> entities)
+        {
+            var duplicates = new List<string>();
+            foreach (var entity in entities)
+            {
+                var method = PropertyExpression.Compile();
+                var text = method(entity as T);
+                if (Get(text) != null)
+                    duplicates.Add(text);
+            }
+            if (duplicates.Count > 0)
+                throw new Exception($"Duplicate {typeof(T).Name}(s) detected: {(string.Join(", ", duplicates))}");
+            return base.Add(entities);
+        }
+    }
+}
