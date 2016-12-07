ff8b436a34c977d8cde9c0dd1574fdc046bc1235
rhyous <rhyous@yahoo.com>
2016-12-07 14:19:35 -0500

    Renamed Entity to IEntity to avoid confusion in docs

--- Rhyous.WebFramework/Interfaces.UserToUserType/IUserToUserTypeExtensions.cs
+++ Rhyous.WebFramework/Interfaces.UserToUserType/IUserToUserTypeExtensions.cs
@@ -3,20 +3,20 @@ using System.Linq;
 
 namespace Rhyous.WebFramework.Interfaces
 {
-    using Entity = IUserToUserType;
+    using IEntity = IUserToUserType;
 
     public static class IUserToUserTypeExtensions
     {
-        public static IEnumerable<T> ToConcrete<T>(this IEnumerable<Entity> items)
-            where T : class, Entity, new()
+        public static IEnumerable<T> ToConcrete<T>(this IEnumerable<IEntity> items)
+            where T : class, IEntity, new()
         {
             return items.Select(i => i.ToConcrete<T>()).ToList();
         }
 
-        public static T ToConcrete<T>(this Entity item)
-            where T : class, Entity, new()
+        public static T ToConcrete<T>(this IEntity item)
+            where T : class, IEntity, new()
         {
-            return ConcreteConverter.ToConcrete<T, Entity>(item);
+            return ConcreteConverter.ToConcrete<T, IEntity>(item);
         }
     }
 }
