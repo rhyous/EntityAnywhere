33336c819f3a34a32230cc1639d245148e0d7033
rhyous <rhyous@yahoo.com>
2017-08-09 13:22:30 -0600

    Added placeholder for variable. Variable must be manually replaced today.

--- templates/EntityAnywhereTemplates/EntityAnywhere.Interface/Extensions.cs
+++ templates/EntityAnywhereTemplates/EntityAnywhere.Interface/Extensions.cs
@@ -5,7 +5,7 @@ namespace Rhyous.WebFramework.Interfaces
 {
     using EntityInterface = IEntity1;
 
-    public static class ISkuExtensions
+    public static class I$EntityNameHere$Extensions
     {
         public static IEnumerable<T> ToConcrete<T>(this IEnumerable<EntityInterface> items)
             where T : class, EntityInterface, new()
