46a99451e3c383a5a18cb55d0c10a62c64e21be6
rhyous <rhyous@yahoo.com>
2017-09-05 17:23:56 -0600

    Fix base class name

--- templates/EntityAnywhereTemplates/EntityAnywhere.Entity/Entity1.cs
+++ templates/EntityAnywhereTemplates/EntityAnywhere.Entity/Entity1.cs
@@ -3,7 +3,10 @@ using Rhyous.WebFramework.Interfaces;
 
 namespace Rhyous.WebFramework.Entities
 {
-    public class Entity1 : AuditableEntityBase<int>, IEntity1
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Entity1 : AuditableEntity<int>, IEntity1
     {
         public string Name { get; set; }
         public string Description { get; set; }
