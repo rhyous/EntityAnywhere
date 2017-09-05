18071f08e0965fe8b0bccbee13366517da300379
rhyous <rhyous@yahoo.com>
2017-09-05 17:23:02 -0600

    Adding documentation to code - Day 3

--- src/Interfaces/Interfaces.User/IUser.cs
+++ src/Interfaces/Interfaces.User/IUser.cs
@@ -1,14 +1,31 @@
 ﻿namespace Rhyous.WebFramework.Interfaces
 {
     /// <summary>
-    /// The user entity.
+    /// The interface for a User entity.
     /// </summary>
     public partial interface IUser : IEntity<long>, IAuditable, IEnabled
     {
+        /// <summary>
+        /// The username used to login as this user.
+        /// </summary>
         string Username { get; set; }
+        /// <summary>
+        /// The password used to login as this user.
+        /// </summary>
         string Password { get; set; }
+        /// <summary>
+        /// The salt that is appended to the password before it is hashed.
+        /// </summary>
         string Salt { get; set; }
+        /// <summary>
+        /// A bool value. If true, the password is salted and hashed. If false, the password is in clear text.
+        /// </summary>
         bool IsHashed { get; set; }
+        /// <summary>
+        /// If true, this user was created by using an Authenticator plugin other than <see cref="Rhyous.WebFramework.Authenticators.User.UserCredentialsValidator"/> which authenticates using the User entity.
+        /// If this is true, the password and salt should be null or empty and IsHashed should be ignored.
+        /// However, a user might want to login using mutiple methods. Once a user is created, if ForceExternalUsersToAuthenticateExternally is false, then a user might be enabled to set their password, and log in externally or internally.
+        /// </summary>
         bool ExternalAuth { get; set; }
     }
 }
